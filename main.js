9aa597d7cd03c911b2b1d627d2f00c5c99d06dd7
ksanaxx <skyricee@mail.ru>
2025-01-26 17:40:02 +0300

    blog

---
+++ main.js
@@ -0,0 +1,5 @@
+document.documentElement.style.setProperty('--scrollbar-width', '12px');
+document.documentElement.style.setProperty('--scrollbar-height', '12px');
+document.documentElement.style.setProperty('--thumb-bg', 'rgba(0, 0, 0, 0.7)');
+document.documentElement.style.setProperty('--track-bg', 'transparent');
+document.documentElement.style.setProperty('--radius', '10px');
